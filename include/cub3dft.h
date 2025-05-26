490dc954dc25543e939be2f7b8e72ba9bd0d8448
Ana Zubieta Pena <azubieta@c1r12s2.42malaga.com>
2025-05-26 13:43:26 +0200

    modificando .h

--- include/cub3dft.h
+++ include/cub3dft.h
@@ -1,9 +1,9 @@
 #ifndef CUB3DFT_H
-# define CUB3D_H
+# define CUB3DFT_H
 
 //libreries
-# include "MLX42.h" // O cualquier archivo de cabecera dentro de MLX42
-# include "libft.h" // O cualquier archivo de cabecera dentro de libft
+# include "../MLX42/include/MLX42/MLX42.h" // O cualquier archivo de cabecera dentro de MLX42
+# include "../libft/libft.h" // O cualquier archivo de cabecera dentro de libft
 # include <stdio.h>
 
 #endif
