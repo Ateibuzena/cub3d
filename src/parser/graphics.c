1cf44beb8dca522163080a5320bd6640f9e251cb
Ana Zubieta Pena <azubieta@c3r5s1.42malaga.com>
2025-07-08 19:19:46 +0200

    norminette graphics

--- src/parser/graphics.c
+++ src/parser/graphics.c
@@ -1,32 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   graphics.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: azubieta <azubieta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/08 19:17:34 by azubieta          #+#    #+#             */
+/*   Updated: 2025/07/08 19:19:30 by azubieta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../include/cub3dft.h"
 
 int	ft_parse_rgb(char *line)
 {
-	char	**parts;
-	int		r, g, b;
-	unsigned int color;
+	char			**parts;
+	unsigned int	color;
+	t_rgb			rgb;
 
 	parts = ft_split(line, ',');
 	if (!parts || ft_strlen_double(parts) != 3)
+	{
+		if (parts)
+			ft_freedouble(parts);
 		return (ft_putstr_fd("Error: Color: not valid\n", 2), -1);
-	r = ft_atoi(parts[0]);
-	g = ft_atoi(parts[1]);
-	b = ft_atoi(parts[2]);
-	if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255)
+	}
+	rgb.r = ft_atoi(parts[0]);
+	rgb.g = ft_atoi(parts[1]);
+	rgb.b = ft_atoi(parts[2]);
+	if (rgb.r < 0 || rgb.r > 255
+		|| rgb.g < 0 || rgb.g > 255
+		|| rgb.b < 0 || rgb.b > 255)
+	{
+		ft_freedouble(parts);
 		return (ft_putstr_fd("Error: Color: out of range (0-255)\n", 2), -1);
-	color = (r << 24) | (g << 16) | (b << 8) | 0xFF;
+	}
+	color = (rgb.r << 24) | (rgb.g << 16) | (rgb.b << 8) | 0xFF;
 	ft_freedouble(parts);
 	return (color);
 }
 
-int ft_is_configuration(char *line)
+int	ft_is_configuration(char *line)
 {
-	return (ft_strncmp(line, "NO ", 3) == 0
-		|| ft_strncmp(line, "SO ", 3) == 0
-		|| ft_strncmp(line, "WE ", 3) == 0
-		|| ft_strncmp(line, "EA ", 3) == 0
-		|| ft_strncmp(line, "F ", 2) == 0
-		|| ft_strncmp(line, "C ", 2) == 0);
+	return (ft_strncmp(line, "NO ", 3) == 0 || ft_strncmp(line, "SO ", 3) == 0
+		|| ft_strncmp(line, "WE ", 3) == 0 || ft_strncmp(line, "EA ", 3) == 0
+		|| ft_strncmp(line, "F ", 2) == 0 || ft_strncmp(line, "C ", 2) == 0);
 }
 
 int	ft_parse_textures(char *line, t_data *data)
@@ -77,8 +95,9 @@ int	ft_parse_colors(char *line, t_data *data)
 
 int	ft_parse_configuration(char **lines, t_data *data)
 {
-	int i = 0;
+	int	i;
 
+	i = 0;
 	while (lines[i] && ft_is_configuration(lines[i]))
 	{
 		if (!ft_parse_textures(lines[i], data))
