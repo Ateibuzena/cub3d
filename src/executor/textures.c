d4ab5457a8cb09e186ef65b8908e816625b5dfe8
Ana Zubieta Pena <azubieta@c3r5s1.42malaga.com>
2025-07-08 18:58:57 +0200

    norminette a textures.c

--- src/executor/textures.c
+++ src/executor/textures.c
@@ -1,19 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   textures.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: azubieta <azubieta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/08 15:01:42 by azubieta          #+#    #+#             */
+/*   Updated: 2025/07/08 18:58:23 by azubieta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../include/cub3dft.h"
 
 int	ft_load_textures(t_game *game, t_data data)
 {
-	game->textures.north = mlx_load_png(data.paths.north);
-	game->textures.south = mlx_load_png(data.paths.south);
-	game->textures.east = mlx_load_png(data.paths.east);
-	game->textures.west = mlx_load_png(data.paths.west);
-
-	if (!game->textures.north || !game->textures.south
-		|| !game->textures.east || !game->textures.west)
+	game->textures.north = ft_validate_textures(data.paths.north,
+			&game->textures.north_xpm);
+	if (!game->textures.north)
+		fprintf(stderr, "Error: Failed to load NORTH texture: %s\n",
+			data.paths.north);
+	game->textures.south = ft_validate_textures(data.paths.south,
+			&game->textures.south_xpm);
+	if (!game->textures.south)
+		fprintf(stderr, "Error: Failed to load SOUTH texture: %s\n",
+			data.paths.south);
+	game->textures.east = ft_validate_textures(data.paths.east,
+			&game->textures.east_xpm);
+	if (!game->textures.east)
+		fprintf(stderr, "Error: Failed to load EAST texture: %s\n",
+			data.paths.east);
+	game->textures.west = ft_validate_textures(data.paths.west,
+			&game->textures.west_xpm);
+	if (!game->textures.west)
+		fprintf(stderr, "Error: Failed to load WEST texture: %s\n",
+			data.paths.west);
+	if (!game->textures.north || !game->textures.south || !game->textures.east
+		|| !game->textures.west)
 		return (0);
+	game->tile_size = game->textures.north->width;
 	return (1);
 }
 
-mlx_texture_t *ft_get_texture(t_game *game, char face)
+mlx_texture_t	*ft_get_texture(t_game *game, char face)
 {
 	if (face == 'N')
 		return (game->textures.north);
@@ -26,15 +54,14 @@ mlx_texture_t *ft_get_texture(t_game *game, char face)
 	return (NULL);
 }
 
-// Calcula step y tex_pos inicial para el mapeo de la textura en la pared
-void ft_init_mapping(t_wall *wall)
+void	ft_init_mapping(t_wall *wall)
 {
 	wall->step = (float)wall->texture->height / wall->height;
-	wall->tex_pos = (wall->start_y - HEIGHT / 2 + wall->height / 2) * wall->step;
+	wall->tex_pos = (wall->start_y - HEIGHT / 2 + wall->height / 2)
+		* wall->step;
 }
 
-// Obtiene el color de la textura en la posición (tex_x, tex_y)
-uint32_t ft_get_color(t_wall *wall, int tex_y)
+uint32_t	ft_get_color(t_wall *wall, int tex_y)
 {
 	int			pixel_index;
 	uint32_t	max_index;
@@ -50,28 +77,9 @@ uint32_t ft_get_color(t_wall *wall, int tex_y)
 	if ((uint32_t)(pixel_index + 3) >= max_index)
 	{
 		fprintf(stderr, "Pixel index out of bounds: %d\n", pixel_index);
-		return (0); // Color negro por defecto ante error
+		return (0);
 	}
 	p = &wall->texture->pixels[pixel_index];
 	color = (p[0] << 24) | (p[1] << 16) | (p[2] << 8) | p[3];
 	return (color);
-}
-
-// Dibuja la columna vertical de la pared en la pantalla
-void ft_draw_wall(t_game *game, int i, t_wall *wall)
-{
-	int y;
-	int tex_y;
-	uint32_t color;
-
-	ft_init_mapping(wall);
-	y = wall->start_y;
-	while (y < wall->end_y)
-	{
-		tex_y = (int)wall->tex_pos;
-		color = ft_get_color(wall, tex_y);
-		mlx_put_pixel(game->img, i, y, color);
-		wall->tex_pos += wall->step;
-		y++;
-	}
 }
