5561791f7e6a228af1a899a7d02ea47950bb5041
Ana Zubieta Pena <azubieta@c3r3s1.42malaga.com>
2025-07-08 14:47:47 +0200

    norminette raycasting.c

--- src/executor/raycasting.c
+++ src/executor/raycasting.c
@@ -1,43 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   raycasting.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: azubieta <azubieta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/08 14:40:32 by azubieta          #+#    #+#             */
+/*   Updated: 2025/07/08 14:46:54 by azubieta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../include/cub3dft.h"
 
-// Inicializa dirección del rayo, posición en el mapa y delta_distancias
-void ft_init_ray(t_ray *ray, t_player *player, float ray_angle)
+void	ft_init_ray(t_ray *ray, t_player *player, float ray_angle)
 {
 	ray->ray_dir_x = cos(ray_angle);
 	ray->ray_dir_y = sin(ray_angle);
-	ray->map_x = (int)(player->x / TILE_SIZE);
-	ray->map_y = (int)(player->y / TILE_SIZE);
+	ray->map_x = (int)(player->x / player->tile_size);
+	ray->map_y = (int)(player->y / player->tile_size);
 	ray->delta_dist_x = fabs(1.0 / ray->ray_dir_x);
 	ray->delta_dist_y = fabs(1.0 / ray->ray_dir_y);
 }
 
-// Inicializa paso y distancia inicial al próximo lado (side_dist)
-void ft_init_step(t_ray *ray, t_player *player)
+void	ft_init_step(t_ray *ray, t_player *player)
 {
 	if (ray->ray_dir_x < 0)
 	{
 		ray->step_x = -1;
-		ray->side_dist_x = (player->x - ray->map_x * TILE_SIZE) / TILE_SIZE * ray->delta_dist_x;
+		ray->side_dist_x = (player->x - ray->map_x * player->tile_size)
+			/ player->tile_size * ray->delta_dist_x;
 	}
 	else
 	{
 		ray->step_x = 1;
-		ray->side_dist_x = ((ray->map_x + 1) * TILE_SIZE - player->x) / TILE_SIZE * ray->delta_dist_x;
+		ray->side_dist_x = ((ray->map_x + 1) * player->tile_size - player->x)
+			/ player->tile_size * ray->delta_dist_x;
 	}
 	if (ray->ray_dir_y < 0)
 	{
 		ray->step_y = -1;
-		ray->side_dist_y = (player->y - ray->map_y * TILE_SIZE) / TILE_SIZE * ray->delta_dist_y;
+		ray->side_dist_y = (player->y - ray->map_y * player->tile_size)
+			/ player->tile_size * ray->delta_dist_y;
 	}
 	else
 	{
 		ray->step_y = 1;
-		ray->side_dist_y = ((ray->map_y + 1) * TILE_SIZE - player->y) / TILE_SIZE * ray->delta_dist_y;
+		ray->side_dist_y = ((ray->map_y + 1) * player->tile_size - player->y)
+			/ player->tile_size * ray->delta_dist_y;
 	}
 }
 
-// Realiza una iteración del algoritmo DDA, avanza el rayo hasta que choca con un muro
-void ft_perform_dda(t_ray *ray, t_game *game)
+void	ft_perform_dda(t_ray *ray, t_game *game)
 {
 	while (1)
 	{
@@ -53,52 +66,51 @@ void ft_perform_dda(t_ray *ray, t_game *game)
 			ray->map_y += ray->step_y;
 			ray->side = 1;
 		}
-		if (ray->map_y >= 0 && ray->map_y < game->map.height
-			&& ray->map_x >= 0 && ray->map_x < game->map.width
+		if (ray->map_y >= 0 && ray->map_y < game->map.height && ray->map_x >= 0
+			&& ray->map_x < game->map.width
 			&& game->map.grid[ray->map_y][ray->map_x] != 0)
 		{
-			ray->wall_type = game->map.grid[ray->map_y][ray->map_x]; // Guarda el tipo de muro
+			ray->wall_type = game->map.grid[ray->map_y][ray->map_x];
 			break ;
 		}
 	}
 }
 
-
-// Calcula el punto de impacto y la distancia corregida para evitar fisheye
-t_hit ft_calculate_hit(t_player *player, t_ray *ray, float ray_angle)
+t_hit	ft_calculate_hit(t_hit *hit, t_player *player, t_ray *ray, float angle)
 {
-	t_hit hit;
-	float wall_dist;
-
 	if (ray->side == 0)
 	{
-		wall_dist = ((ray->map_x * TILE_SIZE - player->x) + (1 - ray->step_x) * TILE_SIZE / 2.0) / ray->ray_dir_x;
-		hit.face = (ray->ray_dir_x > 0) ? 'W' : 'E';
+		hit->wall_dist = ((ray->map_x * player->tile_size - player->x) + (1
+					- ray->step_x) * player->tile_size / 2.0) / ray->ray_dir_x;
+		if (ray->ray_dir_x > 0)
+			hit->face = 'W';
+		else
+			hit->face = 'E';
 	}
 	else
 	{
-		wall_dist = ((ray->map_y * TILE_SIZE - player->y) + (1 - ray->step_y) * TILE_SIZE / 2.0) / ray->ray_dir_y;
-		hit.face = (ray->ray_dir_y > 0) ? 'N' : 'S';
+		hit->wall_dist = ((ray->map_y * player->tile_size - player->y) + (1
+					- ray->step_y) * player->tile_size / 2.0) / ray->ray_dir_y;
+		if (ray->ray_dir_y > 0)
+			hit->face = 'N';
+		else
+			hit->face = 'S';
 	}
-
-	hit.x = player->x + wall_dist * ray->ray_dir_x;
-	hit.y = player->y + wall_dist * ray->ray_dir_y;
-
-	// Corregir distancia para evitar efecto fisheye
-	hit.distance = wall_dist * cos(ray_angle - player->angle);
-	if (hit.distance < 0.1)
-		hit.distance = 0.1;
-
+	hit->x = player->x + hit->wall_dist * ray->ray_dir_x;
+	hit->y = player->y + hit->wall_dist * ray->ray_dir_y;
+	hit->distance = hit->wall_dist * cos(angle - player->angle);
+	if (hit->distance < 0.1)
+		hit->distance = 0.1;
 	return (hit);
 }
 
-// Función principal que usa las anteriores para realizar el raycast completo
-t_hit ft_cast_ray(t_game *game, float ray_angle)
+t_hit	ft_cast_ray(t_game *game, float ray_angle)
 {
-	t_ray ray;
+	t_ray	ray;
+	t_hit	hit;
 
 	ft_init_ray(&ray, &game->player, ray_angle);
 	ft_init_step(&ray, &game->player);
 	ft_perform_dda(&ray, game);
-	return (ft_calculate_hit(&game->player, &ray, ray_angle));
+	return (ft_calculate_hit(&hit, &game->player, &ray, ray_angle));
 }
