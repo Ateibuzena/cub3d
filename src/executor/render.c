a41359ae01e6acd5e141b4cc43849d6a74b3e3b8
Ana Zubieta Pena <azubieta@c3r3s1.42malaga.com>
2025-07-08 15:01:22 +0200

    norminette render.c

--- src/executor/render.c
+++ src/executor/render.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: azubieta <azubieta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/08 15:00:45 by azubieta          #+#    #+#             */
+/*   Updated: 2025/07/08 15:01:08 by azubieta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../include/cub3dft.h"
 
-static int ft_compute_end(int start_y, float height)
+static int	ft_compute_end(int start_y, float height)
 {
-	int end_y;
+	int	end_y;
 
 	end_y = start_y + height;
 	if (end_y > HEIGHT)
@@ -10,9 +22,9 @@ static int ft_compute_end(int start_y, float height)
 	return (end_y);
 }
 
-static int ft_compute_y(float height)
+static int	ft_compute_y(float height)
 {
-	int start_y;
+	int	start_y;
 
 	start_y = (HEIGHT - height) / 2;
 	if (start_y < 0)
@@ -20,19 +32,19 @@ static int ft_compute_y(float height)
 	return (start_y);
 }
 
-static int ft_compute_texture(t_hit hit)
+static int	ft_compute_texture(t_hit hit)
 {
-	int tex_x;
-	if (hit.face == 'N')
-		tex_x = TILE_SIZE - ((int)hit.x % TILE_SIZE) - 1;
-	else if (hit.face == 'S')
-		tex_x = (int)hit.x % TILE_SIZE;
-	else if (hit.face == 'E')
-		tex_x = (int)hit.y % TILE_SIZE;
-	else // 'W'
-		tex_x = TILE_SIZE - ((int)hit.y % TILE_SIZE) - 1;
-	return (tex_x);
+	int	tex_x;
 
+	if (hit.face == 'N')
+		tex_x = hit.tile_size - ((int)hit.x % hit.tile_size) - 1;
+	else if (hit.face == 'S')
+		tex_x = (int)hit.x % hit.tile_size;
+	else if (hit.face == 'E')
+		tex_x = (int)hit.y % hit.tile_size;
+	else
+		tex_x = hit.tile_size - ((int)hit.y % hit.tile_size) - 1;
+	return (tex_x);
 }
 
 void	ft_render_wall(t_game *game, float ray_angle, int i)
@@ -41,9 +53,11 @@ void	ft_render_wall(t_game *game, float ray_angle, int i)
 
 	wall.hit = ft_cast_ray(game, ray_angle);
 	wall.texture = ft_get_texture(game, wall.hit.face);
+	wall.hit.tile_size = game->tile_size;
 	if (!wall.texture || !wall.texture->pixels)
 		return (ft_putstr_fd("Error: MLX42 failed to load texture\n", 2));
-	wall.height = (TILE_SIZE / wall.hit.distance) * (WIDTH / (2 * tan(FOV / 2)));
+	wall.height = (wall.hit.tile_size / wall.hit.distance) * (WIDTH / (2
+				* tan(FOV / 2)));
 	wall.start_y = ft_compute_y(wall.height);
 	wall.end_y = ft_compute_end(wall.start_y, wall.height);
 	wall.tex_x = ft_compute_texture(wall.hit);
