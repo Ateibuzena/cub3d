19b66d9c72b2e9b7ff16a30e31ac6e3bf1446313
Ana Zubieta Pena <azubieta@c3r3s1.42malaga.com>
2025-07-08 14:30:33 +0200

    norminette a draw.c

--- src/executor/draw.c
+++ src/executor/draw.c
@@ -1,33 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   draw.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: azubieta <azubieta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/08 14:22:35 by azubieta          #+#    #+#             */
+/*   Updated: 2025/07/08 14:29:36 by azubieta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../include/cub3dft.h"
 
-void    ft_draw_square(int x, int y, int size, int color, t_game *game)
+void	ft_draw_point(t_numbers p, int size, int color, t_game *game)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (i < size)
-    {
-        mlx_put_pixel(game->img, x + i, y, color);
-        i++;
-    }
-    i = 0;
-    while (i < size)
-    {
-        mlx_put_pixel(game->img, x, y + i, color);
-        i++;
-    }
-    i = 0;
-    while (i < size)
-    {
-        mlx_put_pixel(game->img, x + size, y + i, color);
-        i++;
-    }
-    i = 0;
-    while (i < size)
-    {
-        mlx_put_pixel(game->img, x + i, y + size, color);
-        i++;
-    }
+	i = 0;
+	while (i < size)
+		mlx_put_pixel(game->img, p.x + i++, p.y, color);
+	i = 0;
+	while (i < size)
+		mlx_put_pixel(game->img, p.x, p.y + i++, color);
+	i = 0;
+	while (i < size)
+		mlx_put_pixel(game->img, p.x + size, p.y + i++, color);
+	i = 0;
+	while (i < size)
+		mlx_put_pixel(game->img, p.x + i++, p.y + size, color);
+}
+
+void	ft_draw_wall(t_game *game, int i, t_wall *wall)
+{
+	int			y;
+	int			tex_y;
+	uint32_t	color;
+
+	ft_init_mapping(wall);
+	y = wall->start_y;
+	while (y < wall->end_y)
+	{
+		tex_y = (int)wall->tex_pos;
+		color = ft_get_color(wall, tex_y);
+		mlx_put_pixel(game->img, i, y, color);
+		wall->tex_pos += wall->step;
+		y++;
+	}
 }
 
 void	ft_clear_image(t_game *game)
@@ -42,9 +60,9 @@ void	ft_clear_image(t_game *game)
 		while (x < WIDTH)
 		{
 			if (y < HEIGHT / 2)
-				mlx_put_pixel(game->img, x, y, game->ceiling); // cielo (azul claro)
+				mlx_put_pixel(game->img, x, y, game->ceiling);
 			else
-				mlx_put_pixel(game->img, x, y, game->floor); // suelo (verde oscuro)
+				mlx_put_pixel(game->img, x, y, game->floor);
 			x++;
 		}
 		y++;
