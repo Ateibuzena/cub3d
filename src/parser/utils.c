5decf1a913fe0b4f6a7b2fbc1b60bc02ec52ca7b
Ana Zubieta Pena <azubieta@c3r5s1.42malaga.com>
2025-07-08 19:29:46 +0200

    norminette utils.c

--- src/parser/utils.c
+++ src/parser/utils.c
@@ -1,9 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: azubieta <azubieta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/08 19:24:13 by azubieta          #+#    #+#             */
+/*   Updated: 2025/07/08 19:28:56 by azubieta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../include/cub3dft.h"
 
 int	ft_strlen_nospace(char *str)
 {
-	int i = 0;
-	int count = 0;
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	if (!str)
+		return (0);
 	while (str[i])
 	{
 		if (str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
@@ -13,26 +30,41 @@ int	ft_strlen_nospace(char *str)
 	return (count);
 }
 
-void	ft_free_textures(t_game *game)
+mlx_texture_t	*ft_load_png(const char *path)
 {
-	if (game->textures.north)
-		mlx_delete_texture(game->textures.north);
-	if (game->textures.south)
-		mlx_delete_texture(game->textures.south);
-	if (game->textures.east)
-		mlx_delete_texture(game->textures.east);
-	if (game->textures.west)
-		mlx_delete_texture(game->textures.west);
+	mlx_texture_t	*texture;
+
+	texture = mlx_load_png(path);
+	if (!texture)
+	{
+		ft_putstr_fd("[❌] Failed to load PNG texture: ", 2);
+		ft_putstr_fd((char *)path, 2);
+		ft_putstr_fd("\n", 2);
+	}
+	else
+	{
+		ft_putstr_fd("[✅] PNG texture loaded successfully: ", 1);
+		ft_putstr_fd((char *)path, 1);
+		ft_putstr_fd("\n", 1);
+	}
+	return (texture);
 }
 
-void	ft_free_paths(t_data *data)
+mlx_texture_t	*ft_load_xpm(const char *path, xpm_t **xpm_storage)
 {
-	if (data->paths.north)
-		free(data->paths.north);
-	if (data->paths.south)
-		free(data->paths.south);
-	if (data->paths.east)
-		free(data->paths.east);
-	if (data->paths.west)
-		free(data->paths.west);
+	xpm_t	*xpm;
+
+	xpm = mlx_load_xpm42(path);
+	if (!xpm)
+	{
+		ft_putstr_fd("[❌] Failed to load XPM texture: ", 2);
+		ft_putstr_fd((char *)path, 2);
+		ft_putstr_fd("\n", 2);
+		return (NULL);
+	}
+	*xpm_storage = xpm;
+	ft_putstr_fd("[✅] XPM texture loaded successfully: ", 1);
+	ft_putstr_fd((char *)path, 1);
+	ft_putstr_fd("\n", 1);
+	return (&xpm->texture);
 }
